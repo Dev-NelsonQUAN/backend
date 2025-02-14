b6c6dd35484cf55798c439324c2a7411a497c189
Nelson <nelson.ade.quadri@gmail.com>
2025-02-14 15:50:28 +0100

    Added book management DB

---
+++ healthCareBackEnd/classWorkMongoose/index.js
@@ -0,0 +1,161 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv/config");
+const port = process.env.PORT4;
+
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/booksDB")
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log("Error connecting to Server", err);
+  });
+
+const booksSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  publishedYear: Number,
+  genre: String,
+  available: Boolean,
+});
+
+const booksModel = mongoose.model("books", booksSchema);
+
+app.get("/books", async (req, res) => {
+  try {
+    const getAllBooks = await booksModel.find();
+    res
+      .status(200)
+      .json({ message: "All books gotten Successfully", getAllBooks });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", error: err.message });
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const getOneBook = await booksModel.findById(req.params.id);
+    if (!getOneBook) {
+      res.status(404).json({ message: "Book not found" });
+    }
+    res.status(200).json({ message: "Book gotten", getOneBook });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", error: err.message });
+  }
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    const { title, author, publishedYear, genre, available } = req.body;
+    const createBook = await booksModel.create({
+      title,
+      author,
+      publishedYear,
+      genre,
+      available,
+    });
+
+    if (!title && !author && !publishedYear && !genre && !available) {
+      res.status(409).json({ message: "All fields required" });
+    }
+
+    res.status(200).json({ message: "Book created successfully", createBook });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", err });
+  }
+});
+
+app.patch("/books/:id", async (req, res) => {
+  try {
+    const { title, author, publishedYear, genre, available } = req.body;
+    const updateOneBook = await booksModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        author,
+        publishedYear,
+        genre,
+        available,
+      },
+      { new: true }
+    );
+
+    if (!updateOneBook) {
+      res.status(409).json({ message: "Book not found for update" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Book Updated Successfully", updateOneBook });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", err });
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const findABookAndDelete = await booksModel.findByIdAndDelete(
+      req.params.id
+    );
+
+    if (!findABookAndDelete) {
+      res.status(409).json({ message: "Book not found for deletion" });
+    }
+
+    res.status(200).json({ message: "Book deleted Successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", err });
+  }
+});
+
+app.patch("/books/:id/toogle-availability", async (req, res) => {
+  try {
+    const { available } = req.body;
+    const updateAvailabliity = await booksModel.findByIdAndUpdate(
+      req.params.id,
+      available,
+      { new: true }
+    );
+
+    if (!updateAvailabliity) {
+      res.status(409).json({ message: "Book not found" });
+    }
+
+    if (updateAvailabliity.available === true) {
+      res.status(200).json({
+        message: "Books Availability is successfully changed to true",
+      });
+    }
+    if (updateAvailabliity.available === false) {
+      res.status(409).json({
+        message: "Books Availability is successfully changed to false",
+      });
+    }
+
+    res.status(409).json({ message: " Status updated" });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", error: err.message });
+  }
+});
+
+app.all("/books/delete-all", async (req, res) => {
+  try {
+    const deleteAll = await booksModel.deleteMany();
+    res.status(409).json({ message: " All books deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", err });
+  }
+});
+
+app.all("*", () => {
+  res.status(404).json({ message: "No route matched" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to http://localhost:${port}`);
+});
---
+++ healthCareBackEnd/loginMongoose/index.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+require("dotenv/config");
+const port = process.env.PORT3;
+
+mongoose
+  .connect("mongodb://localhost:27017/aWholeDB")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("An error occurred", err);
+  });
+
+const logUser = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const logUserModel = mongoose.model("loggedUsers", logUser);
+
+app.get("/get-all", async (req, res) => {
+  try {
+    const getAllLogUsers = await logUserModel.find();
+    res.status(200).json({ message: "Gotten all users", getAllLogUsers });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", err });
+  }
+});
+
+app.post("/create-user", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const checkEmailExistence = await logUserModel.findOne({ email });
+
+    if (!checkEmailExistence) {
+      res.status(409).json({ message: "Email exists" });
+    }
+
+    if (!name && !email && password) {
+      res.status(409).json({ message: "All fields are required" });
+    }
+
+    const createUser = await logUserModel.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(200).json({ message: "Created Successfully", createUser });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", err: err.message });
+  }
+});
+
+app.get("/get-one/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getOneUser = await logUserModel.findOne(id);
+
+    if (!findOne) {
+      res.status(409).json({ message: "No user found" });
+    }
+    res.status(200).json({ message: "User successfully gotten", getOneUser });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const loginUser = await logUserModel.findOne({ email });
+
+    if (!loginUser || loginUser.password !== password) {
+      res.status(401).json({ message: "Wrong Email or password" });
+    }
+    res.status(500).json({
+      message: "Loggedin Successful",
+      name: loginUser.name,
+      _id: loginUser.id,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", err });
+  }
+});
+
+app.patch("/update/:id", async (req, res) => {
+  try {
+    // const { name, email, password } = req.body;
+    const updateUser = await logUserModel.findByIdAndDelete(
+      req.params.id,
+      {
+        name,
+        email,
+        password,
+      },
+      { new: true }
+    );
+
+    if (!updateUser) {
+      res.status(409).json({ message: "Can't find user" });
+    }
+    res.status(200).json({ message: "Updated successfully", updateUser });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", err });
+  }
+});
+
+app.all("/delete-all", async (req, res) => {
+  try {
+    const deleteAll = await logUserModel.deleteMany();
+    res.status(200).json({ message: "Deleted Successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", err });
+  }
+});
+
+app.all("*", (req, res) => {
+  res
+    .status(404)
+    .json({
+      message:
+        "You must be lost and out of your sense, Infact how did you get here?",
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to http://localhost:${port}`);
+});
